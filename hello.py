7983daa63e1db43baf7fa2697bc2980fc59ac765
Mohammed Pasha <mk.pasha@hotmail.com>
2026-05-27 00:43:13 -0400

    fix: moment libbrary usage for live time refresh

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap5
 from flask_moment import Moment
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -9,7 +9,7 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.now(timezone.utc))
 
 
 @app.route('/user/<name>')
