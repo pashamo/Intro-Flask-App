29900001e0199400db9d46a0248ea9632b664c9a
Mohammed Pasha <mk.pasha@hotmail.com>
2026-05-27 00:31:49 -0400

    add error handling, bootstrap and moment libraries

--- hello.py
+++ hello.py
@@ -1,12 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello, World! Your browser is {}.</h1>'.format(user_agent)
+    return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
